774b9f5e8d96ac30b681d7e4d61fde3d0c512c52
RafaelZabotto <rafaelm.zabotto@gmail.com>
2021-05-17 23:03:34 -0300

    Criando Interceptor

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -32,8 +32,6 @@ export class CategoriasPage {
     this.categoriaService.findAll().subscribe(response => {
       this.itens = response;
     }),
-    error => {
-      console.log(error);
-    };
+    error => {};
   }
 }
